5a9477a63f5a75d5e037e73ffe6779cfbdc16fc9
liwenzhen <D5209421@163.com>
2024-06-19 11:09:34 +0800

    feat：修复aes错误

--- lib/lor/lib/utils/aes.lua
+++ lib/lor/lib/utils/aes.lua
@@ -1,4 +1,3 @@
--- from lua-resty-session
 local setmetatable = setmetatable
 local tonumber     = tonumber
 local aes          = require "resty.aes"
@@ -25,7 +24,7 @@ local CIPHER_SIZES = {
 local defaults = {
     size   = CIPHER_SIZES[var.session_aes_size] or 256,
     mode   = CIPHER_MODES[var.session_aes_mode] or "cbc",
-    hash   = hashes[var.session_aes_hash]       or "sha512",
+    hash   = hashes[var.session_aes_hash]       or "sha256",
     rounds = tonumber(var.session_aes_rounds)   or 1
 }
 
@@ -38,19 +37,43 @@ function cipher.new(config)
     return setmetatable({
         size   = CIPHER_SIZES[a.size or defaults.size]   or 256,
         mode   = CIPHER_MODES[a.mode or defaults.mode]   or "cbc",
-        hash   = hashes[a.hash       or defaults.hash]   or hashes.sha512,
+        hash   = hashes[a.hash       or defaults.hash]   or hashes.sha256,
         rounds = tonumber(a.rounds   or defaults.rounds) or 1
     }, cipher)
 end
 
-function cipher:encrypt(content, key, iv)
-    local aes_cbc = assert(aes:new(key, nil, aes.cipher(128, "cbc"), {iv = iv}))
-    return aes_cbc:encrypt(content)
+-- 处理salt长度，salt要么为nil要么长度为8
+local function salt_substring(str)
+    if str == nil or string.len(str) < 8 then
+        return nil
+    end
+
+    local str_len = string.len(str)
+    if str_len == 8 then
+        return str
+    else
+        return string.sub(str, 1, 8)
+    end
 end
 
-function cipher:decrypt(content, key, iv)
-    local aes_cbc = assert(aes:new(key, nil, aes.cipher(128, "cbc"), {iv = iv}))
-    return aes_cbc:decrypt(content)
+function cipher:encrypt(content, key, salt)
+    -- 适配salt超过8位出错
+    salt = salt_substring(salt)
+    local _aes,err = aes:new(key, salt, cip(self.size, self.mode), self.hash, self.rounds)
+    if err then
+        ngx.log(ngx.ERR,"aes.new error: ",err)
+    end
+    return _aes:encrypt(content)
+end
+
+function cipher:decrypt(content, key, salt)
+    -- 适配salt超过8位出错
+    salt = salt_substring(salt)
+    local _aes,err = aes:new(key, salt, cip(self.size, self.mode), self.hash, self.rounds)
+    if err then
+        ngx.log(ngx.ERR,"aes.new error: ",err)
+    end
+    return _aes:decrypt(content)
 end
 
 return cipher
